ca340ab85005718280ec21acc55af725929a7807
aurelie ambal <ambalaurelie@gmail.com>
2017-07-17 15:47:05 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -8,34 +8,31 @@ use Auth;
 
 class AdminLoginController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
 
-  public function __construct()
-  {
-    $this->middleware('guest:admin');
-  }
+    public function showLoginForm()
+    {
+        return view('auth.admin-login');
+    }
 
-  public function showLoginForm()
-  {
-      return view('auth.admin-login');
-  }
-
-  public function login(Request $request)
-  {
-    //validate the form DOMCdataSection
+    public function login(Request $request)
+    {
+        //validate the form DOMCdataSection
     $this->validate($request, [
       'email' => 'required|email',
-      'password' => 'required|min:6'
+      'password' => 'required|min:6',
     ]);
 
     //Attemp to log the Admin in
-    if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)){
-      //If successful, then redirect to their intended location
+    if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+        //If successful, then redirect to their intended location
       return redirect()->intended(route('admin.dashboard'));
     }
 
     //If unsuccessful, then redirect back to the login with the form data
     return redirect()->back()->withInput($request->ony('email', 'remember'));
-
-  }
-
+    }
 }
