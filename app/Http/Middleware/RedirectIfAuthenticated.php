29b8a9bc1f92a9d79a182d601f4e0b47f0277d96
aurelie ambal <ambalaurelie@gmail.com>
2017-09-01 15:27:43 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,9 +17,8 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-      if (Auth::guard($guard)->check()) {
-
-        switch ($guard) {
+        if (Auth::guard($guard)->check()) {
+            switch ($guard) {
           case 'admin':
             return redirect()->route('admin.dashboard');
             break;
@@ -30,9 +29,8 @@ class RedirectIfAuthenticated
             return redirect('/home');
             break;
         }
+        }
 
-      }
-
-      return $next($request);
-     }
+        return $next($request);
+    }
 }
