87482c7133fd2f5558c1a441a28b230b4063f9be
souvir <ambalaurelie@gmail.com>
2017-12-06 12:14:01 +0100

    Upgrade dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,28 +14,28 @@ const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
 
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+  .sass('resources/assets/sass/app.scss', 'public/css');
 
- // Full API
- // mix.js(src, output);
- // mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
- // mix.extract(vendorLibs);
- // mix.sass(src, output);
- // mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
- // mix.less(src, output);
- // mix.stylus(src, output);
- // mix.browserSync('my-site.dev');
- // mix.combine(files, destination);
- // mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
- // mix.copy(from, to);
- // mix.copyDirectory(fromDir, toDir);
- // mix.minify(file);
- // mix.sourceMaps(); // Enable sourcemaps
- // mix.version(); // Enable versioning.
- // mix.disableNotifications();
- // mix.setPublicPath('path/to/public');
- // mix.setResourceRoot('prefix/for/resource/locators');
- // mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// Full API
+// mix.js(src, output);
+// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
+// mix.less(src, output);
+// mix.stylus(src, output);
+// mix.browserSync('my-site.dev');
+// mix.combine(files, destination);
+// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
+// mix.copy(from, to);
+// mix.copyDirectory(fromDir, toDir);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.setResourceRoot('prefix/for/resource/locators');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
 
 mix.webpackConfig({
   module: {
@@ -68,11 +68,11 @@ mix.webpackConfig({
     } ),
   ],
 }); //<-- Override webpack.config.js, without editing the file directly.
- // mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
- // mix.options({
- //   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
- //   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
- //   purifyCss: false, // Remove unused CSS selectors.
- //   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
- //   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
- // });
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
+// mix.options({
+//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
+//   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+//   purifyCss: false, // Remove unused CSS selectors.
+//   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+//   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
+// });
